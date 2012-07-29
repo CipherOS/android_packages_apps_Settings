6adff1fe7fdc6600d62dae77c5cda51708b873b4
xoomdev <xoomdevnet@gmail.com>
2012-07-29 09:20:28 -0400

    Settings: Show full proc/version information
    patch set 2: view unformatted kernel version on clicking
      the formatted kernel version
    
    [ Stallix - Evo X ]
    
    * Adapted for KernelVersionPreferenceController on Pie
    
    Change-Id: I2b211fb72c25cc119f2a464b04821fcef77a8908
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreferenceController.java
@@ -24,6 +24,13 @@ import androidx.preference.Preference;
 
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.DeviceInfoUtils;
+import androidx.preference.Preference;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
