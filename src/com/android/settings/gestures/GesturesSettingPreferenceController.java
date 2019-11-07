737b2e6bb75b0f20a58cb32923a724548cc7c4f0
ghbhaha <ghbhaha@gmail.com>
2019-11-07 19:06:00 +0100

    Settings: Add three-fingers-swipe to screenshot [2/2]
    Signed-off-by: ghbhaha <ghbhaha@gmail.com>
    Change-Id: I8d5671870830a561311aa4c6f2d0d2fb14e382d6

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -60,6 +60,7 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
                 new AmbientDisplayConfiguration(context);
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
 
+        controllers.add(new SwipeToScreenshotPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new SwipeToNotificationPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new DoubleTwistPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new DoubleTapPowerPreferenceController(context, FAKE_PREF_KEY));
